a61e174ed0531d00167761c34e57cf467dace688
kkkovalov <vlad_kovalov@icloud.com>
2022-08-24 20:28:20 -0700

    Unable to pass the function thru the component. Commiting for further development.

--- components/loginComponents/SignUp.tsx
+++ components/loginComponents/SignUp.tsx
@@ -1,54 +1,69 @@
 import { useState } from "react";
 import { Button } from "@mui/material";
-import {TextField} from "@mui/material";
+import { TextField } from "@mui/material";
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
+import IUser from '../../pages/login';
+
+interface ISignup {
+  handleUserData(newUser: typeof IUser): void;
+}
+
+const SignUp: React.FC<ISignup> = (props) => {
+  
+  //Handle user change and hook state
+  const [user, setUser] = useState({
+    email: '',
+    username: '',
+    password: ''
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({ ...user, [e.target.name]: [e.target.value] });
+  }
 
 
-const SignUp : React.FC = () => {
-    
-    const [user, setUser] = useState({
-            username: '',
-            password: ''
-          });
-      
-          const [showPass, setPass] = useState(false);
+  //Handle password visibility
+  const [showPass, setPass] = useState(false);
+  const showPassword = () => {
+    setPass(!showPass);
+    const password = document.getElementById('user-password');
+    !showPass ? password?.setAttribute('type', 'text') : password?.setAttribute('type', 'password');
+  }
 
-        const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-            setUser({...user, [e.target.name]: [e.target.value]});
-          }
-      
-          const showPassword = () => {
-            setPass(!showPass);
-            const password = document.getElementById('user-password');
-            !showPass ? password?.setAttribute('type', 'text') : password?.setAttribute('type', 'password');
-          }
-    
-        return(
-          <motion.div
-            initial={{ opacity: 0,  y:-20 }}
-            animate={{ opacity: 1,  y:0 }}
-            exit={{ y:-50}}
-            transition={{
-              duration: 0.5,
-              delay: 0.2,
-            }}
-          >
-        <div className=" h-[15rem] grid align-middle justify-center text-center mt-5" >
-                  <TextField id="outlined-basic" name="username" label="Username or email" onChange={handleChange} value={user.username} variant="outlined" />
-                  <div className="">
-                    <TextField type="password" className="" id="user-password" onChange={handleChange} label="Password" variant="outlined" />
-                    {showPass ? <VisibilityIcon className="visibilityOn" onClick={showPassword}/> : <VisibilityOffIcon className="visibilityOff" onClick={showPassword}/>}
-                  </div>
-                  <div className=" grid">
-                    <Button className=" w-40 m-auto mt-5" variant="outlined">Sign Up</Button>
-                  </div>
-                  <a href="" className=" mt-5">Forgot your password?</a>
-                  <hr className="text-black" />
+
+  const handleSignUp = () => {
+
+  }
+
+
+  // ----------------------------------------------------------------------------------------------------------------------------------------
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ y: -50 }}
+      transition={{
+        duration: 0.5,
+        delay: 0.2,
+      }}
+    >
+      <div className=" h-[15rem] grid align-middle justify-center text-center mt-5" >
+        <TextField className="m-1 w-[15em]" id="outlined-basic" name="username" label="Username" onChange={handleChange} value={user.username} variant="outlined" />
+        <TextField className="m-1 w-[15em]" id="outlined-basic" name="email" label="Email" onChange={handleChange} value={user.email} variant="outlined" />
+        <div className="m-1 w-[15em]">
+          <TextField className="w-[15em]" type="password" id="user-password" onChange={handleChange} label="Password" variant="outlined" />
+          {!showPass ? <VisibilityIcon className="visibilityOn" onClick={showPassword} /> : <VisibilityOffIcon className="visibilityOff" onClick={showPassword} />}
         </div>
-        </motion.div>
-    )
+        <div className=" grid">
+          <Button className=" w-40 m-auto mt-5" variant="outlined" onClick={handleSignUp}>Sign Up</Button>
+        </div>
+        <a href="" className=" mt-5">Forgot your password?</a>
+        <hr className="text-black" />
+      </div>
+    </motion.div>
+  )
 }
 
 export default SignUp;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,51 +1,67 @@
 import { NextPage } from "next"
 import { motion, AnimatePresence } from "framer-motion"
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 //Components
 import SignUp from "../components/loginComponents/SignUp";
 import LogIn from "../components/loginComponents/LogIn";
-import {ButtonGroup} from "@mui/material";
+import { ButtonGroup } from "@mui/material";
 import { Button } from "@mui/material";
 //firebase
-import { app, database} from "./index";
-import { collection, addDoc, getFirestore , getDocs} from 'firebase/firestore';
+import { app, database } from "./index";
+import { collection, addDoc, getFirestore, getDocs } from 'firebase/firestore';
 import { Opacity, Translate } from "@mui/icons-material";
 
+export interface IUser {
+  email?: string,
+  username?: string,
+  password: string,
+}
 
+export interface IArg{
+  
+}
 
-const Login : NextPage = () => {
+const Login: NextPage = () => {
 
   // Variables and hooks
-    
   const [mode, setMode] = useState("sign up");
 
-    const userCollection = collection(database, 'users');
-    console.log(userCollection);
+  const [curUser, setCurUser] = useState({
+    email: '',
+    username: '',
+    password: '',
+  })
 
-    useEffect(() => {
-      const getUsers = async () => {
-        const data = await getDocs(userCollection);
-        
-      }
-    }, [])
-// New branch login-test created
+  const updateUserData = (newUser: IUser):void => {
+    setCurUser({...curUser, [newUser.name]: [newUser.value]});
+  }
 
-    return (
-      
-        <div className="flex login-page-Bg h-screen">
-            <title>Login</title>
-            <div className="login-page pt-5 bg-gradient-to-t from-white to-[#FFF89A] rounded-xl">
-                    <div className=" grid">
-                      <h1><a href='/guest'>Vision</a></h1>
-                      <ButtonGroup className=" m-auto" variant="text" aria-label="outlined button group">
-                        <Button type="submit" onClick={()=>setMode("sign up")}>Sign up</Button>
-                        <Button  onClick={()=>setMode("log in")}>Log in</Button>
-                      </ButtonGroup>
-                    </div>  
-                {mode === "sign up" ? <SignUp /> : <LogIn/>}
-            </div>
+
+  // Database Connection
+  const userCollection = collection(database, 'users');
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(userCollection);
+    }
+  }, [])
+  // New branch login-test created
+
+  return (
+
+    <div className="flex login-page-Bg h-screen">
+      <title>Login</title>
+      <div className="login-page pt-5 bg-gradient-to-t from-white to-[#FFF89A] rounded-xl">
+        <div className=" grid">
+          <h1><a href='/guest'>Vision</a></h1>
+          <ButtonGroup className=" m-auto" variant="text" aria-label="outlined button group">
+            <Button type="submit" onClick={() => setMode("sign up")}>Sign up</Button>
+            <Button onClick={() => setMode("log in")}>Log in</Button>
+          </ButtonGroup>
         </div>
-    )
+        {mode === "sign up" ? <SignUp handleUserData={updateUserData} /> : <LogIn />}
+      </div>
+    </div>
+  )
 }
 
 

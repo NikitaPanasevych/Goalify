46c3232d552eb333ad2d3a5e8e8a135e201b5fe9
kkkovalov <vlad_kovalov@icloud.com>
2022-09-01 15:31:07 -0700

    Initial commit to tasks.tsx

--- pages/tasks.tsx
+++ pages/tasks.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from "next"
 
 const Tasks : NextPage = () => {
+
+    //Starting the creation of tasks page
     return (
         <div>
             <h1>This is a Tasks page.</h1>

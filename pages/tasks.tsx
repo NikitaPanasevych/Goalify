2d56ede5362b6879696edef46b2e24ad09bb3952
kkkovalov <vlad_kovalov@icloud.com>
2022-09-02 19:28:26 -0700

    Updated tasks.tsx to fetch user info

--- pages/tasks.tsx
+++ pages/tasks.tsx
@@ -1,6 +1,20 @@
 import { NextPage } from "next"
 
+import {auth, database} from '../firebase_config'
+import { useAuthState } from "react-firebase-hooks/auth"
+import { useEffect } from "react";
+import { getDocs, collection } from "firebase/firestore";
+
 const Tasks : NextPage = () => {
+
+    const [user, loading, error] = useAuthState(auth);
+
+    useEffect(() => {
+        loading ? console.log('Loading page') :
+        user ? () => {
+            const result = getDocs(collection(database, 'users', user?.uid, 'goals'));
+        } : console.log('No user found');
+    }, [user, loading])
 
     //Starting the creation of tasks page
     return (

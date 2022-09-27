84b2077abc6e603b05df9374fe482c6ddfd9aa81
kkkovalov <vlad_kovalov@icloud.com>
2022-09-26 17:00:16 -0700

    Added tasks div when click on Card

---
+++ components/Dashboard/Card.tsx
@@ -0,0 +1,11 @@
+
+const Card : React.FC = () => {
+    // alert('worked');
+    return (
+        <div id="tasksCard" >
+            <h1>Hello World!</h1>
+        </div>
+    )   
+}
+
+export default Card;
--- components/Dashboard/Carousel.tsx
+++ components/Dashboard/Carousel.tsx
@@ -1,3 +1,6 @@
+import Card from './Card';
+import {useState} from 'react';
+
 
 interface ICarouselCard {
     projectTitle : string;
@@ -5,12 +8,21 @@ interface ICarouselCard {
 }
 
 const CarouselCard : React.FC<ICarouselCard> = (props) => {
+
+    const [visibility, setVisibility] = useState(false);
+
+    const handleClick = () => {
+        setVisibility(!visibility);
+    }
     return(
-            <div className="card p-2">
-                <h1 className=" ">{props.projectTitle}</h1>
-                <h2 className="">{props.projectDescr}</h2>
-                <h3>SubTasks 10/10 </h3>
+        <>
+            <div className="card p-2" onClick={handleClick}>
+                    <h1 className=" ">{props.projectTitle}</h1>
+                    <h2 className="">{props.projectDescr}</h2>
+                    <h3>SubTasks 10/10 </h3>
             </div>
+            {visibility ? <Card /> : null}
+        </>
     )
 }
 
